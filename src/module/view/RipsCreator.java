f974c3f0e2eac90a8aa71bafb5157a1b83bc316c
prac-sistemas <prac-sistemas@PRAC-SISTEMAS.cohan.local>
2015-09-18 16:40:17 -0500

    Error secuencia SP_ no trae resultado

--- src/module/view/RipsCreator.java
+++ src/module/view/RipsCreator.java
@@ -5,15 +5,22 @@
  */
 package module.view;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import module.database.Coneccion;
 
@@ -28,6 +35,8 @@ public class RipsCreator extends javax.swing.JFrame {
      */
     public RipsCreator() {
         initComponents();
+        
+        
     }
 
     /**
@@ -48,8 +57,13 @@ public class RipsCreator extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jButton1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setAutoscrolls(true);
 
         jLabel1.setText("Tipo Documento");
 
@@ -68,67 +82,85 @@ public class RipsCreator extends javax.swing.JFrame {
             }
         });
 
+        jLabel5.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel5.setText("Generador RIPs");
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/logotipo.jpg"))); // NOI18N
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(83, 83, 83)
+                        .addGap(91, 91, 91)
+                        .addComponent(jLabel7))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(108, 108, 108)
+                        .addComponent(jLabel5))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel1)
                             .addComponent(jLabel2)
                             .addComponent(jLabel3))
-                        .addGap(30, 30, 30)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(205, 205, 205)
+                        .addGap(100, 100, 100)
                         .addComponent(jButton1)))
-                .addContainerGap(92, Short.MAX_VALUE))
+                .addContainerGap(45, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(38, 38, 38)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(23, 23, 23)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel7)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel2)))
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel3)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jButton1)
-                .addContainerGap(60, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 366, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 193, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        
+        
         if (jTextField1.getText().equals("")) {
             JOptionPane.showMessageDialog(null, "Por favor ingrese el tipo de documento");
         }
@@ -138,67 +170,63 @@ public class RipsCreator extends javax.swing.JFrame {
         if (jTextArea1.getText().equals("")) {
             JOptionPane.showMessageDialog(null, "Por favor ingrese los numeros de las facturas");
         }
-     
+
         int TipoDoc = 0;
         int DatoDos = 0;
-        int Facturas = 0;
+        String Facturas = "";
 
-            
+        try {
+            Coneccion Con = new Coneccion();
+            Connection an = Con.Conectar();
+            TipoDoc = Integer.parseInt(jTextField1.getText());
+            DatoDos = Integer.parseInt(jTextField2.getText());
+            Facturas = jTextArea1.getText();
 
-                try {
-                    Coneccion Con= new Coneccion();
-                    Connection an = Con.Conectar();
-                    TipoDoc = Integer.parseInt(jTextField1.getText());
-                    DatoDos = Integer.parseInt(jTextField2.getText());
-                    Facturas = Integer.parseInt(jTextArea1.getText());
-                    
-                     System.out.println("Tipo: " + TipoDoc);
-                System.out.println("Dato: " + DatoDos);
-                System.out.println("Fa: " + Facturas);
-                
-                CallableStatement cst = an.prepareCall("{call SP_GENERACION_RIPS_EVENTO_SAVIA_US_FACTURA (?,?,?)}");
-                
-                //Parametros entrada procedimiento
-                cst.setString(1, String.valueOf(TipoDoc));
-                cst.setString(2, String.valueOf(DatoDos));
-                cst.setString(3, String.valueOf(Facturas));
-                
-                // Definimos los tipos de los parametros de salida del procedimiento almacenado
-              /*
-                cst.registerOutParameter(1, java.sql.Types.VARCHAR);
-                cst.registerOutParameter(2, java.sql.Types.VARCHAR);
-                cst.registerOutParameter(3, java.sql.Types.VARCHAR);
-                */
-               
-                        
-                ResultSet rs = cst.executeQuery();
+            System.out.println("Tipo: " + TipoDoc);
+            System.out.println("Dato: " + DatoDos);
+            System.out.println("Fa: " + Facturas);
 
-                if(rs.next()){
-                    FileWriter fwriter = new FileWriter("C:\\Users\\prac-sistemas\\Documents\\Reportes\\images\\prueba.csv");
-                    fwriter.write('\ufeff'); //si no se escribe esto, excel no muestra bien el texto con tildes
-                    String strLinea = "";
-                    do{
-                        strLinea = rs.getString(1)+";"+rs.getString(2);
-                        fwriter.write(strLinea);
-                    }while(rs.next());
-                    fwriter.flush();
-                    fwriter.close();
-                }
+            CallableStatement cst = an.prepareCall("{call SP_GENERACION_RIPS_EVENTO_SAVIA_US_FACTURA (?,?,?)}");
 
-                an.close();
-                
-                
-                } catch (Exception ex) {
-                    System.out.println("Error..." + ex.toString() + ex.getMessage());
-                }
-            
-            
+            //Parametros entrada procedimiento
+            cst.setInt(1, Integer.valueOf(TipoDoc));
+            cst.setInt(2, Integer.valueOf(DatoDos));
+            cst.setString(3, String.valueOf(Facturas));
+
+            // Definimos los tipos de los parametros de salida del procedimiento almacenado
+            /*
+            cst.registerOutParameter(1, java.sql.Types.VARCHAR);
+            cst.registerOutParameter(2, java.sql.Types.VARCHAR);
+            cst.registerOutParameter(3, java.sql.Types.VARCHAR);
+            */
+            ResultSet rs = cst.executeQuery();
+            Date Ahora = new Date();
+            SimpleDateFormat MI_FORMATO = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss", Locale.getDefault());
+            String A = MI_FORMATO.format(Ahora);
+            File file = new File("C:\\RIPS\\rip-" + MI_FORMATO.format(Ahora) + ".csv");
+            if (rs.next()) {
+                FileWriter fwriter = new FileWriter(file);
+                fwriter.write('\ufeff'); //si no se escribe esto, excel no muestra bien el texto con tildes
+                String strLinea = "";
+                do {
+                    strLinea = rs.getString(1) + ";" + rs.getString(2);
+                    fwriter.write(strLinea);
+                } while (rs.next());
+                fwriter.flush();
+                fwriter.close();
+            }
            
+            an.close();
+            
+        } catch (Exception ex) {
+            System.out.println("Error..." + ex.toString() + ex.getMessage());
+        }
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
-         * @param args the command line arguments
-         */
+     * @param args the command line arguments
+     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -236,6 +264,8 @@ public class RipsCreator extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
